86d9eb506509cafefcdf25b96d990b599fd72aea
Jack Parker <jprkr96@gmail.com>
2020-01-09 16:07:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,5 +20,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
- p array.min()
+ #p array.min()
+ value =  
+  array.length.times do |index|
+    if array[index] < value
+      value = array[index]
+    end
+  end
+    return value
 end
